c8c77a14e851f9b179595c98a02fd0391a24de88
MJAFort <mjafort@gmail.com>
2014-12-06 12:41:09 -0500

    Look at movelist
    It is beautiful

--- puzzle/Puzzles_Code-Data/Functions/main.m
+++ puzzle/Puzzles_Code-Data/Functions/main.m
@@ -3,6 +3,8 @@ imshow(image);
 puzzlePieces = getPieces(image);
     %Solve_Puzzle(puzzlePieces,2,1);
 [angle, pairIndices] = Solve_Squares(puzzlePieces);
+[ center1, center2, transConst] = centerToEdge(puzzlePieces,pairIndices);
 % pieceList = getPieceInformation();
+[piecelist] = getPieceSquares(center2,pairIndices,angle,transConst);
 % moveList = makeMoves(pieceList);
 % move(moveList);
