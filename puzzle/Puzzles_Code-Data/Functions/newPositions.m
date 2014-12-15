4b44776b51aa26f3227f6b99ebe1b487afaee148
MJAFort <mjafort@gmail.com>
2014-12-14 21:17:40 -0500

    Fixed Values
    Here we go

--- puzzle/Puzzles_Code-Data/Functions/newPositions.m
+++ puzzle/Puzzles_Code-Data/Functions/newPositions.m
@@ -11,16 +11,16 @@ function [piecelist] = newPositions(placements,bezPieces)
 
     for i = 2:1:length(placements)
         alpha(i-1) = placements(i).g_lock(1)*180/pi;  %change motion alpha
-        xMotion(i-1) = (placements(i).g_lock(2))/1000; %change motion x
-        yMotion(i-1) = (placements(i).g_lock(3))/1000;  %change motion y
+        xMotion(i-1) = (placements(i).g_lock(2))/10; %change motion x
+        yMotion(i-1) = (placements(i).g_lock(3))/10;  %change motion y
         %now I'm getting the current locations of all the pieces
         pieceloc = bezPieces{1,i};         %separating each piece
         %averaging x and y values for each piece to determine pick up
         %locations. I'm not sure this is the best way, but for now it
         %should work.
         positions = [sum(pieceloc)/length(pieceloc)];
-        xpos(i-1) = positions(1)/1000;
-        ypos(i-1) = positions(2)/1000;
+        xpos(i-1) = positions(1)/10;
+        ypos(i-1) = positions(2)/10;
     end
     for j = 2:2:length(placements)
         x=1;
